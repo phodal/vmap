195bc7836c1bc07263c0555da8eecdfa8c0fb461
Fengda HUANG <h@phodal.com>
2016-04-01 07:58:20 +0800

    move vip user to v_data.js

---
+++ static/js/v_data.js
@@ -0,0 +1,46 @@
+var VIP_USERS = [
+    {
+        name: "代码家",
+        latLang: [39.9659583, 116.340371]
+    },
+    {
+        name: "justjavac",
+        latLang: [39.4208999,116.3237109]
+    },
+    {
+        name: "阮一峰",
+        latLang: [31.1326943,121.3256528]
+    },
+    {
+        name: "Trinea",
+        latLang: [30.1756407,119.8266363]
+    },
+    {
+        name: "JacksonTian",
+        latLang: [30.1756407,119.7266363]
+    },
+    {
+        name: "lifesinger",
+        latLang: [30.1756407,119.9266363]
+    },
+    {
+        name: "云风",
+        latLang: [23.126275,113.3407774]
+    },
+    {
+        name: "廖雪峰",
+        latLang: [39.9590298,116.3579938]
+    },
+    {
+        name: "Laruence",
+        latLang: [39.9231061,116.4392567]
+    },
+    {
+        name: "ibireme",
+        latLang: [39.9820396,116.3112008]
+    },
+    {
+        name: "Phodal",
+        latLang: [34.2173804, 108.8981328]
+    }
+];

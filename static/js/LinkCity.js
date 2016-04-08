75d085bafbf8fa58d8032d4fc41a56537be4f12e
Fengda HUANG <h@phodal.com>
2016-04-08 11:19:20 +0800

    render the main link city

--- static/js/LinkCity.js
+++ static/js/LinkCity.js
@@ -4,18 +4,45 @@ define(['leaflet', 'js/data', 'jquery'], function (L, Data, $) {
         this.map = map;
     };
 
+    LinkCity.generateMenu = function (data) {
+        var getMenuItem = function (itemData) {
+            var item = $("<li role='presentation'>")
+                .append(
+                    $("<a>", {
+                        href: '#' + itemData.properties.id,
+                        html: itemData.properties.name,
+                        role: 'menuitem'
+                    }));
+            return item;
+        };
+
+        var $menu = $("<ul class='dropdown-menu'></ul>");
+        $.each(data, function () {
+            $menu.append(
+                getMenuItem(this)
+            );
+        });
+
+        return $menu;
+    };
+
     LinkCity.prototype.render = function () {
         var that = this;
 
         var legend = L.control({position: 'topright'});
+
         legend.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'info legend');
-            div.innerHTML = '<div class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">中国<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#">HTML</a></li><li><a href="#">CSS</a></li> <li><a href="#">JavaScript</a></li></ul></div>';
+            var menu = LinkCity.generateMenu(ChinaGeo, div);
+
+            div.innerHTML = '<div class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">中国<span class="caret"></span></button>' + menu.prop("outerHTML") + '</div>';
+
             div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
             return div;
         };
+        
         legend.addTo(that.map);
     };
-    
+
     return LinkCity;
 });
